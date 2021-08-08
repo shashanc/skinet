aff68b1f3b0e2be5d2a416e74bd58ba85f0de61f
shashanc <shanksharma19@gmail.com>
2021-08-08 13:36:23 +0530

    End of section 12

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -5,7 +5,7 @@ import { ShopComponent } from './shop.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [  {path: '', component: ShopComponent},
-{path: ':id', component: ProductDetailsComponent}];
+{path: ':id', component: ProductDetailsComponent, data: {breadcrumb: {alias: 'productDetails'}}}];
 
 @NgModule({
   declarations: [],
